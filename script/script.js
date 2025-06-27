2df05bd26b3a1ff50c17f4ccafdac7aa0e768ee5
Maximiliano Garrido <maximiliano.garrido@ug.uchile.cl>
2025-06-27 01:39:40 -0400

    avance jueves

--- script/script.js
+++ script/script.js
@@ -3,17 +3,8 @@ let images=$(".thumb"); /*$(".thumb")==document.querySelector(".thumb")*/
 let currentIndex=0
 
 images.click(function(){
-    let src_imagen=$(this).attr("src");
-    $(".imagen-modal").attr("src",src_imagen);
-    //.attr("src") sirve para identificar o leer
-    // pero .attr("src",src_imagen) modifica
-    // el primer elemento del argumento es
-    //el atributo a modificar y el segundo su nuevo valor
     $(".modal").fadeIn();
     //activo .modal con anmacion
-
-//index=$(this).attr("data-index");
-    currentIndex=$(this).attr("data-index");
 })
 
 $(".close").click(function(){
@@ -26,16 +17,25 @@ $("#prev").click(function(){
     $(".imagen-modal").attr("src", src_imagen);
 });
 
-$("#next").click(function(){
-    currentIndex = (currentIndex + 1 + images.length) % images.length;
-    let src_imagen = $(images[currentIndex]).attr("src");
-    $('.imagen-modal').attr("src", src_imagen);
-});
-
-/*el truco del % solo funciona si
-los index van de 0 a images.length*/
-
 //OJO con images.length , jquery automaticamente crea
 /* un array con todos los elementso que forman parte
 de una clase por su orden en el arbol html , es decir
 el orden en que se encuentran en el html*/ 
+
+$(document).ready(function() { 
+  $('.reservar-btn').on('click', function() {
+    let titulo = $(this).data('pelicula');
+
+    // Establecer el valor del campo y hacerlo no editable
+    $('#pelicula-nombre').val(titulo);
+
+    // Mostrar el modal (puedes usar un plugin como Bootstrap Modal si prefieres)
+    $('.modal').fadeIn();
+  });
+});
+
+
+
+$(".confirmar, .cancelar").click(function(){
+    $(".modal").fadeOut();
+});
